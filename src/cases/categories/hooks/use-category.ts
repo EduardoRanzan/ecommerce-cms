a28ff5081588c7add683966a2b94af26695d293e
Eduardo Ranzan Ferreira <99151894+EduardoRanzan@users.noreply.github.com>
2025-10-13 21:50:47 -0300

    Aula 13/10

---
+++ src/cases/categories/hooks/use-category.ts
@@ -0,0 +1,36 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { categoryService } from "../services/category.service";
+import { CategoryDTO } from "../dtos/category.dto";
+
+export function useCategory() {
+    return useQuery({
+        queryKey: ['categories'],
+        queryFn: () => categoryService.list(),
+    });
+}
+
+export function useCategoryById(id: string) {
+    return useQuery({
+        queryKey: ['categories', id],
+        queryFn: () => categoryService.getById(id),
+        enabled: !!id,
+    });
+}
+
+export function userCreateCategory(){
+    return useMutation<CategoryDTO, Error, Omit<CategoryDTO, 'id'>>({
+        mutationFn: (category: Omit<CategoryDTO, 'id'>) => categoryService.create(category);
+    });
+}
+
+export function userUpdateCategory(){
+    return useMutation<CategoryDTO, Error, {id: string, category: CategoryDTO}>({
+        mutationFn: ({id, category}) => categoryService.update(id, category)
+    });
+}
+
+export function userDeleteCategory(){
+    return useMutation<void, Error, string>({
+        mutationFn: (id: string) => categoryService.delete(id)
+    });
+}
